bb9c8f16c0e53a6abf428b1903cffa24b2a9aa9a
jin1125 <shogo.jinta@gmail.com>
2021-08-01 15:27:05 +0900

    04

--- 04/script.js
+++ 04/script.js
@@ -1,3 +1,8 @@
 "use strict";
 
-console.log("Hello World!!");
+const ul = document.getElementById("list");
+for (let i = 0; i < 5; i++) {
+  const li = document.createElement("li");
+  li.textContent = `子要素${i}号`;
+  ul.appendChild(li);
+}
