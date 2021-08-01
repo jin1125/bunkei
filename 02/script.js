bc41f98eb38e970c73490e7d1a57b4e42c501b29
jin1125 <shogo.jinta@gmail.com>
2021-08-01 13:53:14 +0900

    first

---
+++ 02/script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const who = "World!!";
+console.log("Hello " + who);
+console.log("Good Morning! " + who);
---
+++ 03/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const bmi = window.prompt('BMIの値を入力してください');
+const p_element = document.getElementById('message');
+p_element.textContent = `あなたのBMIは${bmi}：`;
+
+if (bmi > 25) {
+  p_element.textContent += '肥満です';
+} else if (bmi < 18.5) {
+  p_element.textContent += '低体重です';
+} else {
+  p_element.textContent += '適正体重です';
+}
