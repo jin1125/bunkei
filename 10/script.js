e3283b571d1b9c6a71d76d3a46f5a80e9941de43
jin1125 <shogo.jinta@gmail.com>
2021-08-02 09:17:30 +0900

    gitignore

--- 10/script.js
+++ 10/script.js
@@ -2,3 +2,8 @@
 
 const button = document.querySelector("button");
 console.log(button);
+
+button.addEventListener('click',()=>{
+  const p = document.getElementById('target');
+  p.classList.toggle('baseStyle');
+})
